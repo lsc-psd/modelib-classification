29d60b7f184d43e84b7d5931dd424c72a023d840
LFC-KeiteiGo <keitei_go@lscc.co.jp>
2020-02-27 16:39:33 +0900

    Densenet added

--- Classification/pytorch/model/DenseNet121.py
+++ Classification/pytorch/model/DenseNet121.py
@@ -1,158 +1,114 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from collections import OrderedDict
-from torch import Tensor
-from torch.jit.annotations import List
+
+import math
 
 
-__all__ = ['DenseNet', 'densenet121']
+class DenseNet121(nn.Module):
+    def __init__(self, nClasses, reduction=0.5, growthRate=32,
+                 init_weight=True):
+        super(DenseNet121, self).__init__()
 
-model_urls = {
-    'densenet121': 'https://download.pytorch.org/models/densenet121-a639ec97.pth',
-}
+        self._build(nClasses, reduction, growthRate)
+        if init_weight:
+            self._init_weights()
 
-
-class _DenseLayer(nn.Module):
-    def __init__(self, num_input_features,
-                 growth_rate, bn_size,
-                 drop_rate):
-        super(_DenseLayer, self).__init__()
-        self.norm1 = nn.BatchNorm2d(num_input_features)
+    def _build(self, nClasses, reduction, growthRate):
+        nChannels = 2 * growthRate
+        self.conv1 = nn.Conv2d(3, nChannels, kernel_size=7, padding=3,
+                               stride=2, bias=False)
+        self.bn1 = nn.BatchNorm2d(nChannels)
         self.relu1 = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(num_input_features,
-                               bn_size * growth_rate,
-                               kernel_size=1, stride=1, bias=False)
-        self.norm2 = nn.BatchNorm2d(bn_size * growth_rate)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(bn_size * growth_rate, growth_rate,
-                               kernel_size=3, stride=1, padding=1,
-                               bias=False)
-        self.drop_rate = float(drop_rate)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-    def bn_function(self, inputs):
-        # type: (List[Tensor]) -> Tensor
-        concated_features = torch.cat(inputs, 1)
-        bottleneck_output = self.conv1(self.relu1(self.norm1(concated_features)))
-        return bottleneck_output
+        self.dense1 = self._make_dense(nChannels, growthRate, 6)
+        nChannels += 6 * growthRate
+        nOutChannels = int(math.floor(nChannels * reduction))
+        self.trans1 = Transition(nChannels, nOutChannels)
 
-    def forward(self, x):
-        x = torch.cat(x, 1)
-        x = self.norm1(x)
-        x = self.relu1(x)
-        x = self.conv1(x)
+        nChannels = nOutChannels
+        self.dense2 = self._make_dense(nChannels, growthRate, 12)
+        nChannels += 12 * growthRate
+        nOutChannels = int(math.floor(nChannels * reduction))
+        self.trans2 = Transition(nChannels, nOutChannels)
 
-        x = self.norm2(x)
-        x = self.relu2(x)
-        x = self.conv2(x)
+        nChannels = nOutChannels
+        self.dense3 = self._make_dense(nChannels, growthRate, 24)
+        nChannels += 24 * growthRate
+        nOutChannels = int(math.floor(nChannels * reduction))
+        self.trans3 = Transition(nChannels, nOutChannels)
 
-        if self.drop_rate > 0:
-            x = F.dropout(x, p=self.drop_rate, training=self.training)
-        return x
+        nChannels = nOutChannels
+        self.dense3 = self._make_dense(nChannels, growthRate, 16)
+        nChannels += 16 * growthRate
 
-
-class _DenseBlock(nn.Module):
-    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate):
-        super(_DenseBlock, self).__init__()
-        for i in range(num_layers):
-            layer = _DenseLayer(
-                num_input_features + i * growth_rate,
-                growth_rate=growth_rate,
-                bn_size=bn_size,
-                drop_rate=drop_rate,
-            )
-            setattr(self, f'denselayer{i+1}', layer)
-
-    def forward(self, init_features):
-        features = [init_features]
-        for name, layer in self.children():
-            new_features = layer(features)
-            features.append(new_features)
-        return torch.cat(features, 1)
-
-
-class _Transition(nn.Module):
-    def __init__(self, num_input_features, num_output_features):
-        super(_Transition, self).__init__()
-        self.norm = nn.BatchNorm2d(num_input_features)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv = nn.Conv2d(num_input_features, num_output_features,
-                              kernel_size=1, stride=1, bias=False)
-        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
-
-    def forward(self, x):
-        x = self.norm(x)
-        x = self.relu(x)
-        x = self.conv(x)
-        x = self.pool(x)
-
-
-class DenseNet(nn.Module):
-    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
-                 num_init_features=64, bn_size=4, drop_rate=0,
-                 num_classes=1000):
-
-        super(DenseNet, self).__init__()
-
-        # First convolution
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
-                                padding=3, bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
-            ('relu0', nn.ReLU(inplace=True)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-        ]))
-
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(
-                num_layers=num_layers,
-                num_input_features=num_features,
-                bn_size=bn_size,
-                growth_rate=growth_rate,
-                drop_rate=drop_rate
-            )
-            self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(num_input_features=num_features,
-                                    num_output_features=num_features // 2)
-                self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = num_features // 2
-
-        # Final batch norm
-        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
-
-        # Linear layer
-        self.classifier = nn.Linear(num_features, num_classes)
+        self.bn1 = nn.BatchNorm2d(nChannels)
+        self.fc = nn.Linear(nChannels, nClasses)
 
     def _init_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight)
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
             elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
-                nn.init.constant_(m.bias, 0)
+                m.bias.data.zero_()
+
+    def _make_dense(self, nChannels, growthRate, nDenseBlocks):
+        layers = []
+        for i in range(int(nDenseBlocks)):
+            layers.append(Bottleneck(nChannels, growthRate))
+            nChannels += growthRate
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        features = self.features(x)
-        out = F.relu(features, inplace=True)
-        out = F.adaptive_avg_pool2d(out, (1, 1))
-        out = torch.flatten(out, 1)
-        out = self.classifier(out)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+        out = self.pool1(out)
+        out = self.trans1(self.dense1(out))
+        out = self.trans2(self.dense2(out))
+        out = self.dense3(out)
+        out = torch.squeeze(F.avg_pool2d(F.relu(self.bn1(out)), 8))
+        out = F.log_softmax(self.fc(out))
         return out
 
 
-def _densenet(arch, growth_rate, block_config, num_init_features, pretrained, progress,
-              **kwargs):
-    model = DenseNet(growth_rate, block_config, num_init_features, **kwargs)
-    return model
+class Bottleneck(nn.Module):
+    def __init__(self, nChannels, growthRate):
+        super(Bottleneck, self).__init__()
+        interChannels = 4*growthRate
+        self.bn1 = nn.BatchNorm2d(nChannels)
+        self.conv1 = nn.Conv2d(nChannels, interChannels, kernel_size=1,
+                               bias=False)
+        self.bn2 = nn.BatchNorm2d(interChannels)
+        self.conv2 = nn.Conv2d(interChannels, growthRate, kernel_size=3,
+                               padding=1, bias=False)
+
+    def forward(self, x):
+        out = self.bn1(x)
+        out = F.relu(out)
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = torch.cat((x, out), 1)
+        return out
 
 
-def densenet121(pretrained=False, progress=True, **kwargs):
-    return _densenet('densenet121', 32, (6, 12, 24, 16), 64, pretrained, progress,
-                     **kwargs)
+class Transition(nn.Module):
+    def __init__(self, nChannels, nOutChannels):
+        super(Transition, self).__init__()
+        self.bn1 = nn.BatchNorm2d(nChannels)
+        self.conv1 = nn.Conv2d(nChannels, nOutChannels, kernel_size=1,
+                               bias=False)
 
+    def forward(self, x):
+        out = self.bn1(x)
+        out = F.relu(out)
+        out = self.conv1(out)
+        out = F.avg_pool2d(out, 2)
+        return out
