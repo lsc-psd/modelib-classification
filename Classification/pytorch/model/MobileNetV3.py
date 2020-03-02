eb4f453bb6aa423482b4f4ba79abbef18a5e5981
LFC-KeiteiGo <keitei_go@lscc.co.jp>
2020-03-02 16:58:22 +0900

    MobnetV3 updated

--- Classification/pytorch/model/MobileNetV3.py
+++ Classification/pytorch/model/MobileNetV3.py
@@ -2,24 +2,68 @@ import torch.nn as nn
 import math
 
 
-def _make_divisible(v, divisor, min_value=None):
-    """
-    This function is taken from the original tf repo.
-    It ensures that all layers have a channel number that is divisible by 8
-    It can be seen here:
-    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    :param v:
-    :param divisor:
-    :param min_value:
-    :return:
-    """
-    if min_value is None:
-        min_value = divisor
-    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
-    # Make sure that round down does not go down by more than 10%.
-    if new_v < 0.9 * v:
-        new_v += divisor
-    return new_v
+class MobileNetV3(nn.Module):
+    def __init__(self, num_classes=1000, init_weight=True):
+        super(MobileNetV3, self).__init__()
+        # setting of inverted residual blocks
+
+        self._build(num_classes)
+        if init_weight:
+            self._initialize_weights()
+
+    def _build(self, num_classes):
+
+        l = list()
+        l.append(conv_3x3_bn(3, 16, 2))
+        l.append(InvertedResidual(16, 3, 16, 16, 0, 0, 1))
+        l.append(InvertedResidual(16, 3, 64, 24, 0, 0, 2))
+        l.append(InvertedResidual(16, 3, 72, 24, 0, 0, 1))
+        l.append(InvertedResidual(16, 5, 72, 40, 1, 0, 2))
+        l.append(InvertedResidual(16, 5, 120, 40, 1, 0, 1))
+        l.append(InvertedResidual(16, 5, 120, 40, 1, 0, 1))
+        l.append(InvertedResidual(16, 3, 240, 80, 0, 1, 2))
+        l.append(InvertedResidual(16, 3, 200, 80, 0, 1, 1))
+        l.append(InvertedResidual(16, 3, 184, 80, 0, 1, 1))
+        l.append(InvertedResidual(16, 3, 184, 80, 0, 1, 1))
+        l.append(InvertedResidual(16, 3, 480, 112, 1, 1, 1))
+        l.append(InvertedResidual(16, 3, 672, 112, 1, 1, 1))
+        l.append(InvertedResidual(16, 5, 672, 160, 1, 1, 1))
+        l.append(InvertedResidual(16, 5, 672, 160, 1, 1, 2))
+        l.append(InvertedResidual(16, 5, 960, 160, 1, 1, 1))
+        self.features = nn.Sequential(*l)
+
+        self.conv = conv_1x1_bn(160, 960)
+        self.avgpool = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            h_swish()
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(960, 1280),
+            h_swish(),
+            nn.Linear(1280, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.conv(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
 
 
 class h_sigmoid(nn.Module):
@@ -75,7 +119,8 @@ def conv_1x1_bn(inp, oup):
 
 
 class InvertedResidual(nn.Module):
-    def __init__(self, inp, hidden_dim, oup, kernel_size, stride, use_se, use_hs):
+    # exp_size, c, use_se, use_hs, s
+    def __init__(self, inp, kernel_size, hidden_dim, oup, use_se, use_hs, stride):
         super(InvertedResidual, self).__init__()
         assert stride in [1, 2]
 
@@ -115,86 +160,3 @@ class InvertedResidual(nn.Module):
             return x + self.conv(x)
         else:
             return self.conv(x)
-
-
-class MobileNetV3(nn.Module):
-    def __init__(self, cfgs, mode, num_classes=1000, width_mult=1.):
-        super(MobileNetV3, self).__init__()
-        # setting of inverted residual blocks
-        self.cfgs = cfgs
-        assert mode in ['large', 'small']
-
-        # building first layer
-        input_channel = _make_divisible(16 * width_mult, 8)
-        layers = [conv_3x3_bn(3, input_channel, 2)]
-        # building inverted residual blocks
-        block = InvertedResidual
-        for k, exp_size, c, use_se, use_hs, s in self.cfgs:
-            output_channel = _make_divisible(c * width_mult, 8)
-            layers.append(block(input_channel, exp_size, output_channel, k, s, use_se, use_hs))
-            input_channel = output_channel
-        self.features = nn.Sequential(*layers)
-        # building last several layers
-        self.conv = nn.Sequential(
-            conv_1x1_bn(input_channel, _make_divisible(exp_size * width_mult, 8)),
-            SELayer(_make_divisible(exp_size * width_mult, 8)) if mode == 'small' else nn.Sequential()
-        )
-        self.avgpool = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),
-            h_swish()
-        )
-        output_channel = _make_divisible(1280 * width_mult, 8) if width_mult > 1.0 else 1280
-        self.classifier = nn.Sequential(
-            nn.Linear(_make_divisible(exp_size * width_mult, 8), output_channel),
-            nn.BatchNorm1d(output_channel) if mode == 'small' else nn.Sequential(),
-            h_swish(),
-            nn.Linear(output_channel, num_classes),
-            nn.BatchNorm1d(num_classes) if mode == 'small' else nn.Sequential(),
-            h_swish() if mode == 'small' else nn.Sequential()
-        )
-
-        self._initialize_weights()
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.conv(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                n = m.weight.size(1)
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
-
-
-# inp, hidden_dim, oup, kernel_size, stride, use_se, use_hs)
-cfgs = [
-    # k, t, c, SE, NL, s
-    [3,  16,  16, 0, 0, 1],
-    [3,  64,  24, 0, 0, 2],
-    [3,  72,  24, 0, 0, 1],
-    [5,  72,  40, 1, 0, 2],
-    [5, 120,  40, 1, 0, 1],
-    [5, 120,  40, 1, 0, 1],
-    [3, 240,  80, 0, 1, 2],
-    [3, 200,  80, 0, 1, 1],
-    [3, 184,  80, 0, 1, 1],
-    [3, 184,  80, 0, 1, 1],
-    [3, 480, 112, 1, 1, 1],
-    [3, 672, 112, 1, 1, 1],
-    [5, 672, 160, 1, 1, 1],
-    [5, 672, 160, 1, 1, 2],
-    [5, 960, 160, 1, 1, 1]
-]
