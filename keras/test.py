602cac959b2669392fa5789723ae9cfb8148b699
lsc-psd <60456267+lsc-psd@users.noreply.github.com>
2020-03-26 15:50:09 +0900

    make test.py

---
+++ keras/test.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main(args):
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-config', default='./last_config.pickle', type=str, help='config file for models')
+    parser.add_argument('-ckpt', type=str, help='checkpoint')
+    parser.add_argument('-tags_csv', type=str, help='tags_csv file')
+    parser.add_argument('-f', default='test_imgs', type=str, help='data folder path')
+    args = parser.parse_args()
+    main(args)
