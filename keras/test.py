d7015d0d7f43f8e1ce248491bc7eced25ce602f2
lsc-psd <lscpsdpsd@gmail.com>
2020-03-27 13:30:31 +0900

    make test.py

--- keras/test.py
+++ keras/test.py
@@ -1,19 +1,47 @@
-import argparse
+from PIL import Image
+from importlib import import_module
+import csv
 
 
-def main(args):
-    
-    model = System()
-    model = model.load_model()
+def writeResultsInCsv(name, prediction):
 
-    
-    
+    appendData = [name, prediction]
+    csvPath = name + '.csv'
+    if not os.path.exists(csvPath):
+        with open(csvPath, 'w') as f:
+            writer = csv.writer(f, lineterminator='\n')
+            writer.writerow(appendData)
+    else:
+        with open(csvPath, 'a') as f:
+            writer = csv.writer(f, lineterminator='\n')
+            writer.writerow(appendData)
+
+def test(read_default):
+
+    image_path = read_default.get('test_dir')
+    img_height = int(read_default.get('img_height'))
+    img_width = int(read_default.get('img_width'))
+    input_shape = (img_height, img_width, 3)
+    n_categories = int(read_default.get('n_categories'))
+    model_name = read_default.get('model_name')
+    weigth_path = read_default.get('weight_path')
+
+    image = Image.open(image_path).resize((img_height, img_width), Image.LANCZOS)
+    image = np.array(image)
+
+    Structure = import_module(f'models.{model_name}')
+    model = Structure.build(input_shape, n_categories)
+    model.load_weights(weigth_path)
+    prediction = model.predict(image)
+
+    name = os.path.basename(image_path)
+    writeResultsInCsv(name, prediction)
+    print(name, prediction)
     
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-config', default='./last_config.pickle', type=str, help='config file for models')
-    parser.add_argument('-ckpt', type=str, help='checkpoint')
-    parser.add_argument('-tags_csv', type=str, help='tags_csv file')
-    parser.add_argument('-f', default='test_imgs', type=str, help='data folder path')
-    args = parser.parse_args()
-    main(args)
+
+    config_ini = configparser.ConfigParser()
+    config_ini.read('config.ini', encoding='utf-8')
+    read_default = config_ini['DEFAULT']
+
+    test(read_default)
