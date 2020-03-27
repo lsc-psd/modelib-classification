e89ba8c2e22bc0648bfce07171e91ac01eaad138
lsc-psd <lscpsdpsd@gmail.com>
2020-03-27 15:28:16 +0900

    make test.py

--- keras/test.py
+++ keras/test.py
@@ -1,24 +1,28 @@
+import os
+import glob
+import numpy as np
 from PIL import Image
 from importlib import import_module
 import csv
+import configparser
 
+def writeResultsInCsv(data_array):
 
-def writeResultsInCsv(name, prediction):
-
-    appendData = [name, prediction]
-    csvPath = name + '.csv'
+    csvPath = 'test_data.csv'
     if not os.path.exists(csvPath):
         with open(csvPath, 'w') as f:
             writer = csv.writer(f, lineterminator='\n')
-            writer.writerow(appendData)
+            for data in data_array:
+                writer.writerow(data)
     else:
         with open(csvPath, 'a') as f:
             writer = csv.writer(f, lineterminator='\n')
-            writer.writerow(appendData)
+            for data in data_array:
+                writer.writerow(data)
 
 def test(read_default):
 
-    image_path = read_default.get('test_dir')
+    test_path = read_default.get('test_dir')
     img_height = int(read_default.get('img_height'))
     img_width = int(read_default.get('img_width'))
     input_shape = (img_height, img_width, 3)
@@ -26,17 +30,22 @@ def test(read_default):
     model_name = read_default.get('model_name')
     weigth_path = read_default.get('weight_path')
 
-    image = Image.open(image_path).resize((img_height, img_width), Image.LANCZOS)
-    image = np.array(image)
-
     Structure = import_module(f'models.{model_name}')
     model = Structure.build(input_shape, n_categories)
-    model.load_weights(weigth_path)
-    prediction = model.predict(image)
+    #model.load_weights(weigth_path)
 
-    name = os.path.basename(image_path)
-    writeResultsInCsv(name, prediction)
-    print(name, prediction)
+    image_path = glob.glob(os.path.join(test_path, "*"))
+    data = []
+    for path in image_path:
+        image = Image.open(path).resize((img_height, img_width), Image.LANCZOS)
+        image = np.array(image)
+        prediction = model.predict([[image]])
+        name = os.path.basename(path)
+        data.append([name, prediction])
+        print(str(name) + " : " + str(prediction))
+    import pdb;
+    pdb.set_trace()
+    writeResultsInCsv(data)
     
 if __name__ == '__main__':
 
