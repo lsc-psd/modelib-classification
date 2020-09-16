11077704cc0bc9a42fc4b94da60b57d31ff0f65c
lsc-psd <lscpsdpsd@gmail.com>
2020-09-16 19:42:56 +0900

    2020.9.16 fix

--- keras/test.py
+++ keras/test.py
@@ -10,51 +10,59 @@ import imghdr
 import argparse
 
 
-def writeResultsInCsv(data_array):
+def writeResultsInCsv(categories, data):
     csvPath = 'test_data.csv'
     if not os.path.exists(csvPath):
         with open(csvPath, 'w') as f:
             writer = csv.writer(f, lineterminator='\n')
-            for data in data_array:
-                writer.writerow(data)
+            writer.writerow(["", categories])
+            writer.writerow(data)
     else:
         with open(csvPath, 'a') as f:
             writer = csv.writer(f, lineterminator='\n')
-            for data in data_array:
-                writer.writerow(data)
+            writer.writerow(data)
 
 def extension_check(filename):
-    if imghdr.what(filename) == 'png':
-        return print("image is png")
-    elif imghdr.what(filename) == 'jpeg':
-        return print("image is jpeg")
-    else:
-        print("Extension is different")
-        sys.exit()
+    for name in filename:
+        if imghdr.what(name) == 'png':
+            return print("image is png")
+        elif imghdr.what(name) == 'jpeg':
+            return print("image is jpeg")
+        elif imghdr.what(name) == 'jpg':
+            return print("image is jpg")
+        else:
+            print("Extension is different")
+            sys.exit()
 
 def test(read_default):
 
-    test_path = read_default.get('test_dir')
+    test_path = glob.glob(os.path.join(read_default.get('test_dir'), "*"))
     img_height = int(read_default.get('img_height'))
     img_width = int(read_default.get('img_width'))
     input_shape = (img_height, img_width, 3)
-    n_categories = len(glob.glob(os.path.join(read_default.get('train_dir'), "*")))
+    categories = glob.glob((os.path.join(read_default.get('train_dir'), "*")))
+    print(categories)
     model_name = read_default.get('model_name')
-    weigh_path = read_default.get('weight_path')
+    weigh_path = glob.glob(os.path.join(read_default.get('weight_path'), "*"))
 
     extension_check(test_path)
-    Structure = import_module(f'models.{model_name}')
-    model = Structure.build(input_shape, n_categories)
-    weight = glob.glob(weigh_path)
-    model.load_weights(weight)
 
-    image = Image.open(test_path).resize((img_height, img_width))
-    image = np.array(image)
-    prediction = model.predict([[image]])
-    name = os.path.basename(test_path)
-    print(str(name) + " : " + str(prediction))
-    data = [name, prediction]
-    writeResultsInCsv(data)
+    Structure = import_module(f'models.{model_name}')
+    model = Structure.build(input_shape, len(categories))
+    model.load_weights(weigh_path[0])
+
+    category_name_list = []
+    for category_name in categories:
+        category_name_list.append(os.path.basename(category_name))
+
+    for path in test_path:
+        image = Image.open(path).resize((img_height, img_width))
+        image = np.array(image)
+        prediction = model.predict([[image]])
+        name = os.path.basename(path)
+        print(str(name) + " : " + str(prediction))
+        data = [name, prediction]
+        writeResultsInCsv(category_name_list, data)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
